d2df5e0476b5ab03a578a30eef425b4f109b8269
JuanPappalardo <juan.gp7@hotmail.com>
2020-06-13 00:29:19 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { NavbarBrand, Navbar } from "reactstrap";
 import "./App.css";
 import Main from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div className="App text-left">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App text-left">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
